588a435cfc1aa2853a335fb566470725a6c80dbd
SigN0ob <pavic.romain@gmail.com>
2021-04-16 01:00:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.config = configs;
 // DEMARRER LE BOT
 /////////////////////////////////////////////////////
 
-client.login(configs.token)
+client.login(process.env.TOKEN)
   .then(
     () => {
       console.log("Bot démarré");
